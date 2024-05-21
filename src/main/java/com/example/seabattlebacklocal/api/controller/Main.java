0cafeb59114170bff4efe34aaf689dfda08d2edb
Yulia <julikostina1504@gmaol.com>
2024-05-21 22:24:03 +0300

    some another changes

---
+++ src/main/java/com/example/seabattlebacklocal/api/controller/Main.java
@@ -0,0 +1,19 @@
+package com.example.seabattlebacklocal.source;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Main class
+ */
+@SpringBootApplication
+public class Main {
+    /**
+     * Main method
+     *
+     * @param args command line arguments
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+}
